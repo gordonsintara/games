a008cf8e6e712eb9b6249febb63fce3b1e635a3a
gordon <gordon@sintara.co.za>
2021-06-04 22:10:19 +0200

    need to finish js

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.getElementsById('score');
+    const width = 28 ;
+    let score = 0 ;
+
+
+    // 0 == pac-food
+    // 1 == wall
+    // 2 == ghost-lair
+    // 3 == power-up
+    // 4 == empty
+
+    const layout = [
+
+
+    ]
+
+    const squares = [] ;
+
+    
+})
